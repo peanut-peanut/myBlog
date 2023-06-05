c95d70997d23c22a350fcbc54ce33eacbafd7c2f
wangchenxu <wangchenxu@baijia.com>
2023-06-05 21:01:55 +0800

    feat: 数据库连接环境配置

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
 // Schema、Model、Entity或者Documents的关系请牢记，Schema生成Model，Model创造Entity，Model和Entity都可对数据库操作造成影响，但Model比Entity更具操作性。
 const mongoose = require('mongoose')
 // 连接数据库 如果不自己创建 默认test数据库会自动生成
+const config = require('./config');
 
-mongoose.connect('mongodb://localhost/peanut', {
+const DB_URL = process.env.NODE_ENV === 'production' ? config.dbUrl : 'mongodb://localhost/peanut';
+
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true
 });
 
 // 为这次连接绑定事件
